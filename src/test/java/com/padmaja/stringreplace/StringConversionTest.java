c43678949143d2de6d882ee058368bbade3eec4a
unknown <raje1reddy@gmail.com>
2019-08-27 13:49:56 +0100

    converting the string from lower to uppercase

--- src/test/java/com/padmaja/stringreplace/StringConversionTest.java
+++ src/test/java/com/padmaja/stringreplace/StringConversionTest.java
@@ -9,16 +9,13 @@ import static org.junit.Assert.assertEquals;
 //import static org.junit.Assert.*;
 
  public class StringConversionTest {
-
      @Test
-     public void replaceTest1() {
+     public void toUpperCase() {
+         StringConversion test = new StringConversion();
+         String result = test.ToUpperCase("padmaja reddy");
+         String expectedresult = "PADMAJA REDDY";
+         assertEquals(expectedresult ,result);
 
-        StringConversion util = new StringConversion();
-         //manipulateString("HelloWorld");
-         String result = util.manipulateString("HelloWorld");
-         //System.out.println(result);
-         String expectedresult = "vbppvzvbpz";
-         assertEquals(expectedresult,result);
      }
  }
 
